a5985655809329a4b69a1e23e11b44cb13b9e45f
Alan Peabody <gapeabody@gmail.com>
2014-12-26 17:41:14 -0500

    Accept POST/PUT/PATCH as application/json.

--- lib/relax/plug_parser.ex
+++ lib/relax/plug_parser.ex
@@ -3,13 +3,22 @@ defmodule Relax.PlugParser do
   alias Plug.Conn
 
   def parse(conn, "application", "vnd.api+json", _headers, opts) do
-    conn
-      |> Conn.read_body(opts)
-      |> decode
+    parse(conn, opts)
+  end
+
+  # Allow "normal" json requests to be handled as well.
+  def parse(conn, "application", "json", _headers, opts) do
+    parse(conn, opts)
   end
 
   def parse(conn, _type, _subtype, _headers, _opts) do
     {:next, conn}
+  end
+
+  defp parse(conn, opts) do
+    conn
+      |> Conn.read_body(opts)
+      |> decode
   end
 
   defp decode({:more, _, conn}) do
