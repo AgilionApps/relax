5fbfea25e1a52199d409072dbfc2c499a8d135c0
Alan Peabody <gapeabody@gmail.com>
2015-07-03 12:43:43 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Relax.Mixfile do
 
   def project do
     [app: :relax,
-     version: "0.0.3",
+     version: "0.1.0",
      elixir: "~> 1.0.0",
      package: package,
      description: description,
